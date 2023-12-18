f4c2d1776c8cfe43bd507db980088e687ad79f70
Павел <@DESKTOP-8BRSR7L>
2023-12-19 01:08:20 +0300

    Добавьте файлы проекта.

---
+++ Karhering/Hub.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Karhering
+{
+    partial class Hub
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            panel1 = new Panel();
+            guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
+            pictureBox1 = new PictureBox();
+            guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            panel2 = new Panel();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
+            guna2Button7 = new Guna.UI2.WinForms.Guna2Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(128, 255, 255);
+            panel1.Controls.Add(guna2Button6);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(guna2Button5);
+            panel1.Controls.Add(guna2Button4);
+            panel1.Controls.Add(guna2Button3);
+            panel1.Controls.Add(guna2Button2);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-1, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(214, 583);
+            panel1.TabIndex = 3;
+            // 
+            // guna2Button6
+            // 
+            guna2Button6.CustomizableEdges = customizableEdges1;
+            guna2Button6.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button6.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button6.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button6.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button6.FillColor = Color.FromArgb(128, 255, 255);
+            guna2Button6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            guna2Button6.ForeColor = Color.Black;
+            guna2Button6.Location = new Point(6, 195);
+            guna2Button6.Name = "guna2Button6";
+            guna2Button6.RightToLeft = RightToLeft.No;
+            guna2Button6.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button6.Size = new Size(196, 45);
+            guna2Button6.TabIndex = 11;
+            guna2Button6.Text = "Автомобили";
+            guna2Button6.TextAlign = HorizontalAlignment.Left;
+            guna2Button6.Click += guna2Button6_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._213123412;
+            pictureBox1.Location = new Point(96, 494);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(118, 86);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2Button5
+            // 
+            guna2Button5.CustomizableEdges = customizableEdges3;
+            guna2Button5.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button5.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button5.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button5.FillColor = Color.FromArgb(128, 255, 255);
+            guna2Button5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            guna2Button5.ForeColor = Color.Black;
+            guna2Button5.Location = new Point(6, 462);
+            guna2Button5.Name = "guna2Button5";
+            guna2Button5.RightToLeft = RightToLeft.No;
+            guna2Button5.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button5.Size = new Size(176, 45);
+            guna2Button5.TabIndex = 10;
+            guna2Button5.Text = "о приложении";
+            guna2Button5.TextAlign = HorizontalAlignment.Left;
+            guna2Button5.Click += guna2Button5_Click;
+            // 
+            // guna2Button4
+            // 
+            guna2Button4.CustomizableEdges = customizableEdges5;
+            guna2Button4.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button4.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button4.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button4.FillColor = Color.FromArgb(128, 255, 255);
+            guna2Button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            guna2Button4.ForeColor = Color.Black;
+            guna2Button4.Location = new Point(6, 368);
+            guna2Button4.Name = "guna2Button4";
+            guna2Button4.RightToLeft = RightToLeft.No;
+            guna2Button4.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button4.Size = new Size(198, 45);
+            guna2Button4.TabIndex = 9;
+            guna2Button4.Text = "Поддержка";
+            guna2Button4.TextAlign = HorizontalAlignment.Left;
+            guna2Button4.Click += guna2Button4_Click;
+            // 
+            // guna2Button3
+            // 
+            guna2Button3.CustomizableEdges = customizableEdges7;
+            guna2Button3.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button3.FillColor = Color.FromArgb(128, 255, 255);
+            guna2Button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            guna2Button3.ForeColor = Color.Black;
+            guna2Button3.Location = new Point(6, 317);
+            guna2Button3.Name = "guna2Button3";
+            guna2Button3.RightToLeft = RightToLeft.No;
+            guna2Button3.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button3.Size = new Size(198, 45);
+            guna2Button3.TabIndex = 8;
+            guna2Button3.Text = "Настройки";
+            guna2Button3.TextAlign = HorizontalAlignment.Left;
+            guna2Button3.Click += guna2Button3_Click;
+            // 
+            // guna2Button2
+            // 
+            guna2Button2.CustomizableEdges = customizableEdges9;
+            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button2.FillColor = Color.FromArgb(128, 255, 255);
+            guna2Button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            guna2Button2.ForeColor = Color.Black;
+            guna2Button2.Location = new Point(6, 255);
+            guna2Button2.Name = "guna2Button2";
+            guna2Button2.RightToLeft = RightToLeft.No;
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Button2.Size = new Size(198, 45);
+            guna2Button2.TabIndex = 7;
+            guna2Button2.Text = "Бонусы и промо";
+            guna2Button2.TextAlign = HorizontalAlignment.Left;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(2, 64);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(212, 96);
+            panel2.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.Location = new Point(3, 17);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 17);
+            label6.TabIndex = 8;
+            label6.Text = "label6";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(10, 16);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 16);
+            label5.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(3, 45);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 16);
+            label4.TabIndex = 6;
+            label4.Text = "Рейтинг - 4,3";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(22, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 33);
+            label1.TabIndex = 4;
+            label1.Text = "InstantCar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(286, 161);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 17);
+            label2.TabIndex = 4;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(128, 255, 255);
+            label3.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(5, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 18);
+            label3.TabIndex = 5;
+            // 
+            // gMapControl1
+            // 
+            gMapControl1.Bearing = 0F;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.EmptyTileColor = Color.Navy;
+            gMapControl1.GrayScaleMode = false;
+            gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            gMapControl1.LevelsKeepInMemory = 5;
+            gMapControl1.Location = new Point(219, 36);
+            gMapControl1.MarkersEnabled = true;
+            gMapControl1.MaxZoom = 2;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MouseWheelZoomEnabled = true;
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl1.Name = "gMapControl1";
+            gMapControl1.NegativeMode = false;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.RetryLoadTile = 0;
+            gMapControl1.RoutesEnabled = true;
+            gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            gMapControl1.SelectedAreaFillColor = Color.FromArgb(33, 65, 105, 225);
+            gMapControl1.ShowTileGridLines = false;
+            gMapControl1.Size = new Size(953, 550);
+            gMapControl1.TabIndex = 6;
+            gMapControl1.Zoom = 0D;
+            gMapControl1.Load += gMapControl1_Load;
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2Button7
+            // 
+            guna2Button7.BackColor = Color.White;
+            guna2Button7.CustomizableEdges = customizableEdges11;
+            guna2Button7.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button7.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button7.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button7.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button7.FillColor = Color.White;
+            guna2Button7.Font = new Font("Segoe UI", 9F);
+            guna2Button7.ForeColor = Color.White;
+            guna2Button7.Image = Properties.Resources.cancel_cross_icon_icons_com_71726;
+            guna2Button7.Location = new Point(1155, 5);
+            guna2Button7.Name = "guna2Button7";
+            guna2Button7.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Button7.Size = new Size(28, 25);
+            guna2Button7.TabIndex = 0;
+            guna2Button7.Click += guna2Button7_Click;
+            // 
+            // Hub
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1184, 598);
+            Controls.Add(guna2Button7);
+            Controls.Add(gMapControl1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Hub";
+            Text = "Hub";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private PictureBox pictureBox1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private Label label5;
+        private Label label6;
+        private Guna.UI2.WinForms.Guna2Button guna2Button6;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Panel panel3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button7;
+    }
+}
---
+++ Karhering/Nastroiki.cs
@@ -0,0 +1,63 @@
+﻿using Guna.UI2.WinForms;
+using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karhering
+{
+    public partial class Nastroiki : Form
+    {
+        public Nastroiki()
+        {
+            InitializeComponent();
+            label6.Text = DataBank.Text2;
+            label6.Text = DataBank.Text3;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Hub log = new Hub();
+            this.Hide();
+            log.Show();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login log = new Login();
+            this.Hide();
+            log.Show();
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            Delete log = new Delete();
+            log.Show();
+        }
+    }
+}
+
